a34d5ea6adbb5bed9de341f282ad73f2b72c0b3d
Максим Балахонов <gmoaokgsliem@gmail.com>
2018-06-22 15:32:31 +0500

    Initial Commit

---
+++ football/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  football
+//
+//  Created by Максим Белугин on 22.06.2018.
+//  Copyright © 2018 Максим Белугин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
